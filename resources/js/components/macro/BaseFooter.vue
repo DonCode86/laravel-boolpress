add1e92cfe88d35053368fe5e4b7a74250d97fff
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-07-27 16:47:50 +0200

    first commit

---
+++ resources/js/components/macro/BaseFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer>
+        <div class="container">Made with ❤️ classe 63</div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "BaseFooter",
+};
+</script>
+
+<style></style>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <BaseHeader />
+        <BaseMain />
+        <BaseFooter />
+        <ul>
+            <li v-for="post in posts" :key="post.slug">
+                {{ post.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import BaseHeader from "../components/macro/BaseHeader.vue";
+import BaseMain from "../components/macro/BaseMain.vue";
+import BaseFooter from "../components/macro/BaseFooter.vue";
+export default {
+    name: "App",
+    components: {
+        BaseHeader,
+        BaseMain,
+        BaseFooter,
+    },
+
+    created() {
+        axios
+            .get("http://127.0.0.1:8000/api/posts")
+            .then((response) => {
+                this.posts = response.data;
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    },
+};
+</script>
+
+<style></style>
