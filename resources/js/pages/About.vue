752d48b6b4ebf4ae8bb2ae54101149704117be4f
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-07-27 17:40:55 +0200

    aggiunta navigazione

---
+++ resources/js/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <h1>Chi siamo</h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex alias
+            dolorem labore ullam corporis consequatur sunt culpa ab corrupti
+            blanditiis incidunt unde doloremque, saepe atque voluptatem amet
+            velit. Porro, ex.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "About",
+};
+</script>
+
+<style></style>
